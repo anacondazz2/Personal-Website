2ddf49e1825d78920d28dce3cce84d8a9c3e040f
Patrick Hu <patrickhu360@outlook.com>
2021-10-18 20:31:47 -0400

    Added reponsive dropdown navbar for small devices

--- index.js
+++ index.js
@@ -8,34 +8,33 @@ const navbar = document.querySelector(".navbar");
 const navbarMenu = document.querySelector(".navbar .menu");
 const hamburger = document.querySelector(".navbar .hamburger");
 
+// hamburger on-click
+let burger_active = false;
+hamburger.addEventListener("click", (event) => {
+   burger_active = !burger_active;
+   navbar.classList.add("darken");
+   navbarMenu.classList.toggle("dropdown");
+   document.querySelector(".navbar .hamburger i").className = burger_active
+      ? "fas fa-times"
+      : "fas fa-bars";
+});
+
+// on scroll
+const navbarOnScroll = () => {
+   if (window.scrollY > 10 || burger_active) {
+      navbar.classList.add("darken");
+   } else {
+      navbar.classList.remove("darken");
+   }
+};
+window.onscroll = navbarOnScroll;    
+
 // "Patrick Hu" on mouse over
 my_name.onclick = (event) => {
    my_name.style.opacity = 1;
-}
+};
 
 // "programmer" on mouse click
 word_programmer.onclick = (event) => {
    word_programmer.style.opacity = 1;
-}
-
-// navbar on scroll
-const navbarOnScroll = () => {
-   if (window.scrollY > 10) {
-      navbar.style.padding = "25px 55px";
-      navbar.style.backgroundColor = "rgba(66, 66, 66, 0.75)";
-   } else {
-      navbar.style.padding = "35px 55px";
-      navbar.style.backgroundColor = "";
-   }
-}
-window.onscroll = navbarOnScroll;
-
-// hamburger on-click
-let btn_active = false;
-hamburger.addEventListener("click", (event) => {
-   btn_active = !btn_active;
-   navbarMenu.style.left = btn_active ? 0 : "-100%";
-   document.querySelector(".navbar .hamburger i").className = btn_active
-      ? "fas fa-times"
-      : "fas fa-bars";
-});
+};
