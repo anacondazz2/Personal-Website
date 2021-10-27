a42fac1a1521a6b6b9523d35bc5877b129a77793
Patrick Hu <patrickhu360@outlook.com>
2021-10-26 22:43:23 -0400

    Fixed navbar responsiveness, added design

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-/** to dos:
- */
-
 // grab needed elements
 const html = document.querySelector("html");
-const my_name = document.getElementById("my-name");
-const word_programmer = document.getElementById("word-programmer");
+const welcomeText = document.querySelector(".home .large-panel-text");
+const goodbyeText = document.querySelector(".footer .large-panel-text");
 const navbar = document.querySelector(".navbar");
 const navbarMenu = document.querySelector(".navbar .menu");
-const hamburger = document.querySelector(".navbar .hamburger");
+const hamburger = document.querySelector(".navbar .hamburger.fas");
+const nav_links = document.querySelectorAll(".navbar .menu li a");
+
+// functions
+const atEndOfPage = () => {
+   return Math.abs(html.scrollHeight - html.scrollTop - html.clientHeight) <= 3.0;
+}
+const updateBurgerIcon = () => {
+   document.querySelector(".navbar .hamburger.fas").className = burger_active
+   ? "hamburger fas fa-times" // 'x' close button
+   : "hamburger fas fa-bars"; // triple bar menu button
+}
 
 // hamburger on-click
 let burger_active = false;
@@ -15,18 +23,24 @@ hamburger.addEventListener("click", (event) => {
    burger_active = !burger_active;
    navbar.classList.add("darken");
    navbarMenu.classList.toggle("dropdown");
-   document.querySelector(".navbar .hamburger i").className = burger_active
-      ? "fas fa-times"
-      : "fas fa-bars";
+   updateBurgerIcon();
 });
+
+// nav-links on-click when burger is active
+for (let i = 0; i < nav_links.length; i++) {
+   nav_links[i].addEventListener("click", (event) => {
+      if (burger_active) {
+         // retract the dropdown menu
+         navbarMenu.classList.remove("dropdown");
+         burger_active = false;
+         updateBurgerIcon();
+      }
+   })
+}
 
 // on scroll
 const navbarOnScroll = () => {
-   // console.log("scroll height", html.scrollHeight);
-   // console.log("scroll top", html.scrollTop);
-   // console.log("client height", html.clientHeight);
-
-   if (Math.abs(html.scrollHeight - html.scrollTop - html.clientHeight) <= 3.0) {
+   if (atEndOfPage()) {
       navbar.classList.remove("darken");
    } else if (window.scrollY > 40 || burger_active) {
       navbar.classList.add("darken");
@@ -36,12 +50,11 @@ const navbarOnScroll = () => {
 };
 window.onscroll = navbarOnScroll;
 
-// "Patrick Hu" on mouse over
-my_name.onclick = (event) => {
-   my_name.style.opacity = 1;
+// home welcome text on hover
+welcomeText.onmouseover = (event) => {
+   welcomeText.style.opacity = 1;
 };
-
-// "programmer" on mouse click
-word_programmer.onclick = (event) => {
-   word_programmer.style.opacity = 1;
-};
+// goodbye text on hover
+goodbyeText.onmouseover = () => {
+   goodbyeText.style.opacity = 1;
+}
